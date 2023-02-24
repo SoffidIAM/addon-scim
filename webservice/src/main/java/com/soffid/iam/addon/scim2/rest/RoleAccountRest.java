0998140883c95bfe1473c2756b0fa26db8a3b78c
Gabriel Buades <gbuades@soffid.com>
2023-02-24 09:54:00 +0100

    IAM-4024 Add link to inherited roles

--- webservice/src/main/java/com/soffid/iam/addon/scim2/rest/RoleAccountRest.java
+++ webservice/src/main/java/com/soffid/iam/addon/scim2/rest/RoleAccountRest.java
@@ -1,13 +1,19 @@
 package com.soffid.iam.addon.scim2.rest;
 
+import javax.ejb.CreateException;
+import javax.naming.NamingException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.json.JSONObject;
+
 import com.soffid.iam.api.Role;
 import com.soffid.iam.api.RoleAccount;
+
+import es.caib.seycon.ng.exception.InternalErrorException;
 
 @Path("/scim2/v1/RoleAccount")
 @Produces({"application/scim+json", "application/json"})
--- webservice/src/main/java/com/soffid/iam/addon/scim2/rest/UserRest.java
+++ webservice/src/main/java/com/soffid/iam/addon/scim2/rest/UserRest.java
@@ -1,25 +1,42 @@
 package com.soffid.iam.addon.scim2.rest;
 
 import java.io.OutputStreamWriter;
+import java.util.Collection;
 
 import javax.ejb.CreateException;
 import javax.json.JsonArray;
 import javax.naming.NamingException;
 import javax.servlet.annotation.HttpConstraint;
 import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.core.Response.Status;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONWriter;
 
 import com.soffid.iam.EJBLocator;
+import com.soffid.iam.addon.scim.response.SCIMResponseBuilder;
+import com.soffid.iam.addon.scim.util.PaginationUtil;
 import com.soffid.iam.addon.scim2.json.JSONBuilder;
 import com.soffid.iam.api.Account;
+import com.soffid.iam.api.PagedResult;
 import com.soffid.iam.api.Password;
+import com.soffid.iam.api.RoleGrant;
 import com.soffid.iam.api.System;
 import com.soffid.iam.api.User;
+import com.soffid.iam.common.security.SoffidPrincipal;
+import com.soffid.iam.utils.Security;
 
 import es.caib.seycon.ng.exception.BadPasswordException;
 import es.caib.seycon.ng.exception.InternalErrorException;
@@ -87,7 +104,57 @@ public class UserRest extends BaseRest<User> {
 
 		addReference (builder, jsonObject, "groupUsers", "GroupUser?filter=user+eq+'"+encode(obj.getUserName())+"'+and+disabled+eq+false");
 
+		addReference (builder, jsonObject, "effectiveGrants", "User/"+encode(obj.getId().toString())+"/effectiveGrants");
+
 		jsonObject.write(w);
+	}
+
+
+	@Path("/{id}/effectiveGrants")
+	@GET
+	public Response list(@PathParam("id") long id, 
+			@Context HttpServletRequest request)
+			throws Throwable {
+		JSONBuilder b = new JSONBuilder(request);
+		Security.nestedLogin( (SoffidPrincipal) ((HttpServletRequest) request).getUserPrincipal());
+		try {
+			Collection<RoleGrant> r = EJBLocator.getApplicationService().findEffectiveRoleGrantByUser(id);
+			
+			return Response.ok( (StreamingOutput) output -> {
+				OutputStreamWriter w = new OutputStreamWriter(output, "UTF-8");
+				JSONWriter jsonWriter = new JSONWriter(w);
+				jsonWriter.object();
+				jsonWriter.key("schemas");
+				{
+					jsonWriter.array();
+					jsonWriter.value("urn:ietf:params:scim:api:messages:2.0:ListResponse");
+					jsonWriter.endArray();
+				}
+				jsonWriter.key("totalResults"); jsonWriter.value(r.size());
+				jsonWriter.key("startIndex"); jsonWriter.value(1);
+				jsonWriter.key("Resources");
+				jsonWriter.array();
+				{
+					boolean first = true;
+					for (RoleGrant grant: r) {
+						if (first) first = false;
+						else w.append(",");
+						JSONObject jsonObject = b.build(grant);
+						jsonObject.write(w);
+					}
+				}
+				jsonWriter.endArray();
+				jsonWriter.endObject();
+				w.close();
+				output.close();
+			}).build();
+		} catch (Exception e) {
+			log.warn("Error processing SCIM Request "+request.getRequestURL(), e);
+			return SCIMResponseBuilder.errorGeneric(e);
+		} finally {
+			Security.nestedLogoff();
+		}
+		
 	}
 
 }
